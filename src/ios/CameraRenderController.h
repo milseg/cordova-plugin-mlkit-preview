b66f5d5ee175caa85aea4b58613a44a89c2b617e
Milton <milton@aiboo.com.br>
2020-05-30 23:08:25 -0300

    Initial

---
+++ src/ios/CameraRenderController.h
@@ -0,0 +1,43 @@
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreMedia/CoreMedia.h>
+#import <CoreVideo/CoreVideo.h>
+#import <QuartzCore/QuartzCore.h>
+#import <CoreImage/CoreImage.h>
+#import <ImageIO/ImageIO.h>
+
+#import "CameraSessionManager.h"
+
+@protocol TakePictureDelegate
+- (void) invokeTakePicture;
+- (void) invokeTakePictureOnFocus;
+@end;
+
+@protocol FocusDelegate
+- (void) invokeTapToFocus:(CGPoint)point;
+@end;
+
+@protocol Base64Helper
+- (NSString *)getBase64Image:(CGImageRef)imageRef withQuality:(CGFloat) quality;
+- (NSString *)getBase64FromCIImage:(CIImage*)imageRef;
+@end;
+
+@interface CameraRenderController : UIViewController <AVCaptureVideoDataOutputSampleBufferDelegate, OnFocusDelegate> {
+  GLuint _renderBuffer;
+  CVOpenGLESTextureCacheRef _videoTextureCache;
+  CVOpenGLESTextureRef _lumaTexture;
+}
+
+@property (nonatomic) CameraSessionManager *sessionManager;
+@property (nonatomic) CIContext *ciContext;
+@property (nonatomic) CIImage *latestFrame;
+@property (nonatomic) EAGLContext *context;
+@property (nonatomic) NSLock *renderLock;
+@property BOOL dragEnabled;
+@property BOOL tapToTakePicture;
+@property BOOL tapToFocus;
+@property (nonatomic, assign) id delegate;
+@property (nonatomic) NSString *frameB64;
+
+@end
